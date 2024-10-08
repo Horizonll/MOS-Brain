3c716c35ccb278c80c6ec48be9ad00403349a8df
hrz <herz23@mails.tsinghua.edu.cn>
2024-10-08 21:08:51 +0800

    重构状态机，添加线程锁以确保线程安全；删除不必要的状态机实例化，更新需求文件

--- state machine/decider.py
+++ state machine/decider.py
@@ -8,6 +8,7 @@ from sensor_msgs.msg import JointState
 from decision_subscriber import Decision_Pos, Decision_Vision, Decision_Motion
 from receiver import Receiver
 from utils import config
+import threading
 
 from transitions import Machine
 import time
@@ -29,10 +30,6 @@ class Agent(Decision_Pos, Decision_Motion, Decision_Vision):
 
         self._state = None
         self.state_machine = StateMachine(self)
-        self.find_ball_state_machine = FindBallStateMachine(self)
-        self.chase_ball_state_machine = ChaseBallStateMachine(self)
-        self.adjust_position_state_machine = AdjustPositionStateMachine(self)
-        self.kick_state_machine = KickStateMachine(self)
         self.ifBall = False
         self.ready_to_kick = False
 
@@ -59,18 +56,22 @@ class Agent(Decision_Pos, Decision_Motion, Decision_Vision):
 
     def find_ball(self):
         print("Looking for the ball")
+        self.find_ball_state_machine = FindBallStateMachine(self)
         self.find_ball_state_machine.run()
 
     def chase_ball(self):
         print("Chasing the ball")
+        self.chase_ball_state_machine = ChaseBallStateMachine(self)
         self.chase_ball_state_machine.run()
 
     def adjust_position(self):
         print("Adjusting position")
+        self.adjust_position_state_machine = AdjustPositionStateMachine(self)
         self.adjust_position_state_machine.run()
 
     def kick(self):
         print("Kicking the ball")
+        self.kick_state_machine = KickStateMachine(self)
         self.kick_state_machine.run()
 
     def no_ball(self):
@@ -182,6 +183,7 @@ class FindBallStateMachine:
             initial="search",
             transitions=self.transitions,
         )
+        self.lock = threading.Lock()
 
     def ball_in_sight(self):
         return self.agent.ball_in_sight()
@@ -189,43 +191,46 @@ class FindBallStateMachine:
     def run(self):
         while self.state != "found":
             print("Searching for the ball...")
-            self.machine.model.trigger("search_ball")
+            with self.lock:
+                self.machine.model.trigger("search_ball")
 
     def search_ball(self):
-        self.agent.stop(0.5)
-        protect_pose = JointState()
-        protect_pose.name = [
-            "L_arm_1",
-            "L_arm_2",
-            "L_arm_3",
-            "R_arm_1",
-            "R_arm_2",
-            "R_arm_3",
-        ]
-        protect_pose.position = [0, 1.2, -0.5, 0, -1.2, 0.5]
-        self.agent.joint_goal_publisher.publish(protect_pose)
+        with self.lock:
+            self.agent.stop(0.5)
+            protect_pose = JointState()
+            protect_pose.name = [
+                "L_arm_1",
+                "L_arm_2",
+                "L_arm_3",
+                "R_arm_1",
+                "R_arm_2",
+                "R_arm_3",
+            ]
+            protect_pose.position = [0, 1.2, -0.5, 0, -1.2, 0.5]
+            self.agent.joint_goal_publisher.publish(protect_pose)
 
-        for pos in range(6):
-            t = time.time()
-            while time.time() - t < 1:
-                if self.agent.ifBall:
-                    return
-                self.agent.head_set(
-                    head=config.find_head_pos[pos], neck=config.find_neck_pos[pos]
-                )
-
-        self.agent.speed_controller(0, 0, config.walk_theta_vel)
-        t = time.time()
-        while self.agent.loop() and time.time() - t < 10:
-            for pos in [0, 3]:
-                t1 = time.time()
-                while time.time() - t1 < 2:
+            for pos in range(6):
+                t = time.time()
+                while time.time() - t < 1:
                     if self.agent.ifBall:
-                        self.agent.stop(0.5)
                         return
                     self.agent.head_set(
                         head=config.find_head_pos[pos], neck=config.find_neck_pos[pos]
                     )
+
+            self.agent.speed_controller(0, 0, config.walk_theta_vel)
+            t = time.time()
+            while self.agent.loop() and time.time() - t < 10:
+                for pos in [0, 3]:
+                    t1 = time.time()
+                    while time.time() - t1 < 2:
+                        if self.agent.ifBall:
+                            self.agent.stop(0.5)
+                            return
+                        self.agent.head_set(
+                            head=config.find_head_pos[pos],
+                            neck=config.find_neck_pos[pos],
+                        )
 
 
 class ChaseBallStateMachine:
@@ -247,6 +252,7 @@ class ChaseBallStateMachine:
             initial="move",
             transitions=self.transitions,
         )
+        self.lock = threading.Lock()
 
     def close_to_ball(self):
         return self.agent.close_to_ball()
@@ -254,18 +260,22 @@ class ChaseBallStateMachine:
     def run(self):
         while self.state != "arrived":
             print("Moving to the ball...")
-            self.machine.model.trigger("move_to_ball")
+            with self.lock:
+                self.machine.model.trigger("move_to_ball")
 
     def move_to_ball(self, ang=0.25):
-        if abs(self.cam_neck) > ang:
-            self.speed_controller(0, 0, np.sign(self.cam_neck) * config.walk_theta_vel)
+        with self.lock:
+            if abs(self.cam_neck) > ang:
+                self.speed_controller(
+                    0, 0, np.sign(self.cam_neck) * config.walk_theta_vel
+                )
 
-        elif abs(self.cam_neck) <= ang:
-            self.motionState = "Forward"
-            self.speed_controller(
-                config.walk_x_vel, 0, 2.5 * self.cam_neck * config.walk_theta_vel
-            )
-            time.sleep(0.1)
+            elif abs(self.cam_neck) <= ang:
+                self.motionState = "Forward"
+                self.speed_controller(
+                    config.walk_x_vel, 0, 2.5 * self.cam_neck * config.walk_theta_vel
+                )
+                time.sleep(0.1)
 
 
 class AdjustPositionStateMachine:
@@ -301,25 +311,30 @@ class AdjustPositionStateMachine:
             initial="angel",
             transitions=self.transitions,
         )
+        self.lock = threading.Lock()
 
     def run(self):
         while self.state != "finished" and self.agent.ifBall:
             print("Adjusting position...")
-            self.machine.model.trigger("adjust_position")
+            with self.lock:
+                self.machine.model.trigger("adjust_position")
 
     def adjust_angel(self):
-        self.agent.head_set(0.05, 0)
-        target_angle_rad = math.atan((self.agent.pos_x - 0) / (4500 - self.agent.pos_y))
-        ang_tar = target_angle_rad * 180 / math.pi
-        ang_delta = ang_tar - self.agent.pos_yaw
-        if ang_delta > 10:
-            self.agent.speed_controller(0, -0.05, 0.3)
+        with self.lock:
+            self.agent.head_set(0.05, 0)
+            target_angle_rad = math.atan(
+                (self.agent.pos_x - 0) / (4500 - self.agent.pos_y)
+            )
+            ang_tar = target_angle_rad * 180 / math.pi
             ang_delta = ang_tar - self.agent.pos_yaw
-            print(f"ang_delta={ang_delta}")
-        elif ang_delta < -10:
-            self.agent.speed_controller(0, 0.05, 0.3)
-            ang_delta = ang_tar - self.agent.pos_yaw
-            print(f"ang_delta={ang_delta}")
+            if ang_delta > 10:
+                self.agent.speed_controller(0, -0.05, 0.3)
+                ang_delta = ang_tar - self.agent.pos_yaw
+                print(f"ang_delta={ang_delta}")
+            elif ang_delta < -10:
+                self.agent.speed_controller(0, 0.05, 0.3)
+                ang_delta = ang_tar - self.agent.pos_yaw
+                print(f"ang_delta={ang_delta}")
 
     def good_angel(self):
         target_angle_rad = math.atan((self.pos_x - 0) / (4500 - self.agent.pos_y))
@@ -328,51 +343,59 @@ class AdjustPositionStateMachine:
         return abs(ang_delta) < 10
 
     def adjust_lr(self):
-        self.agent.head_set(head=0.9, neck=0)
-        self.agent.stop(1)
-        no_ball_count = 0
-        t0 = time.time()
-        while (
-            self.agent.loop() and (self.agent.ball_x < 600) or (self.agent.ball_x == 0)
-        ):
-            if time.time() - t0 > 10 or no_ball_count > 5:
-                return
-            if not self.agent.ifBall:
-                no_ball_count += 1
-                time.sleep(0.7)
-                continue
-            self.agent.speed_controller(0, 0.6 * config.walk_y_vel, 0)
+        with self.lock:
+            self.agent.head_set(head=0.9, neck=0)
+            self.agent.stop(1)
+            no_ball_count = 0
+            t0 = time.time()
+            while (
+                self.agent.loop()
+                and (self.agent.ball_x < 600)
+                or (self.agent.ball_x == 0)
+            ):
+                if time.time() - t0 > 10 or no_ball_count > 5:
+                    return
+                if not self.agent.ifBall:
+                    no_ball_count += 1
+                    time.sleep(0.7)
+                    continue
+                self.agent.speed_controller(0, 0.6 * config.walk_y_vel, 0)
 
-        while (
-            self.agent.loop() and (self.agent.ball_x > 660) or (self.agent.ball_x == 0)
-        ):
-            if time.time() - t0 > 10 or no_ball_count > 5:
-                return
-            if not self.agent.ifBall:
-                no_ball_count += 1
-                time.sleep(0.7)
-                continue
-            self.agent.speed_controller(0, -0.6 * config.walk_y_vel, 0)
-        self.agent.stop(0.5)
+            while (
+                self.agent.loop()
+                and (self.agent.ball_x > 660)
+                or (self.agent.ball_x == 0)
+            ):
+                if time.time() - t0 > 10 or no_ball_count > 5:
+                    return
+                if not self.agent.ifBall:
+                    no_ball_count += 1
+                    time.sleep(0.7)
+                    continue
+                self.agent.speed_controller(0, -0.6 * config.walk_y_vel, 0)
+            self.agent.stop(0.5)
 
     def good_lr(self):
         return 600 <= self.agent.ball_x <= 660
 
     def adjust_fb(self):
-        self.agent.stop(0.5)
-        t0 = time.time()
-        no_ball_count = 0
-        while (
-            self.agent.loop() and (self.agent.ball_y < 420) or (self.agent.ball_y == 0)
-        ):
-            if time.time() - t0 > 10 or no_ball_count > 5:
-                return
-            if not self.agent.ifBall:
-                no_ball_count += 1
-                time.sleep(0.7)
-                continue
-            self.agent.speed_controller(0.5 * config.walk_x_vel, 0, 0)
-        self.agent.stop(0.5)
+        with self.lock:
+            self.agent.stop(0.5)
+            t0 = time.time()
+            no_ball_count = 0
+            while (
+                self.agent.loop()
+                and (self.agent.ball_y < 420)
+                or (self.agent.ball_y == 0)
+            ):
+                if time.time() - t0 > 10 or no_ball_count > 5:
+                    return
+                if not self.agent.ifBall:
+                    no_ball_count += 1
+                    time.sleep(0.7)
+                    continue
+                self.agent.speed_controller(0.5 * config.walk_x_vel, 0, 0)
+            self.agent.stop(0.5)
 
     def good_fb(self):
         self.agent.ready_to_kick = 420 <= self.agent.ball_y
