820dc3a04530fcdb61fe090d318aa6abe3b8caac
zhuoheng22 <zhuoheng22@mails.tsinghua.edu.cn>
2025-04-01 18:57:33 +0800

    allow force look_at

--- decider/client/decider.py
+++ decider/client/decider.py
@@ -38,12 +38,15 @@ from robot_client import RobotClient
 
 class Agent:
     # @public variants:
-    #       VARIANTS             TYPE                 DESCRIPTION
+    #           NONE
     #
     # @public methods:
     #   cmd_vel(vel_x : float, vel_y : float, vel_theta : float)
     #   kick()
-    #   run()
+    #   look_at(head: float, neck: float)
+    #       disable automatically tracking and force to look_at
+    #       use (NaN, NaN) to enable tracking
+    #   
     #
     # @public methods to get varants:
     #       METHODS             TYPE                DESCRIPTION
@@ -166,6 +169,9 @@ class Agent:
         rospy.loginfo(f"Setting the robot's speed: linear velocity x={vel_x}, "
                 + "y={vel_y}, angular velocity theta={vel_theta}")
 
+    def look_at(head: float, neck: float):
+        self._vision.look_at(head, neck)
+    
     def stop(self):
         self._action.cmd_vel(0, 0, 0)
 
