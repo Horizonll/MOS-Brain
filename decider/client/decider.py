d191e3ba76b5d0c971d97edbf2abb17e3cd2859f
luo-sy24 <luo-sy24@mails.tsinghua.edu.cn>
2025-05-03 16:09:00 +0800

    kick config

--- decider/client/decider.py
+++ decider/client/decider.py
@@ -170,10 +170,8 @@ class Agent:
                 self._state_machine_runners['go_back_to_field']()
             else:
                 if time.time() - self.decider_start_time < self.start_walk_into_field_time:
-                    print(f"fuck1 {time.time() - self.decider_start_time}")
                     self._state_machine_runners['go_back_to_field']()
                 elif time.time() - self.penalize_end_time < self.start_walk_into_field_time:
-                    print(f"fuck2 {time.time() - self.penalize_end_time}")
                     self._state_machine_runners['go_back_to_field']()
                 elif time.time() - self._last_play_time < 10.0 \
                         and not self.receiver.kick_off:
