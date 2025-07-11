61e0bdd4edbd8b016bca7154b46d01fa2c603484
thmos <thmos>
2025-07-12 01:02:14 +0800

    support unitree systemd

---
+++ decider/scripts/g1/head_control.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source $HOME/Workspace/unitree_ros2/setup.sh
+source $HOME/Workspace/THMOS/thmos-msgs/install/setup.sh
+export PATH=/usr/local/cuda-11.4/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1:$LD_PRELOAD
+
+/usr/bin/python3 ~/Workspace/THMOS/head-control/head_control.py
